de2c55cb3ac50e1f238d0061b4d15d6212bf7e9e
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-10-09 15:44:27 +0600

    fix send email

--- app/config/settings.py
+++ app/config/settings.py
@@ -161,7 +161,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.example.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
