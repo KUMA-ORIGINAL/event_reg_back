b0218c1dad2eee3608ca3e262d377705d51521ab
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-10-09 16:08:46 +0600

    fix send email

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 from django.core.mail import send_mail
 from rest_framework import viewsets, mixins, status
@@ -5,31 +7,35 @@ from rest_framework.response import Response
 
 from api.serializers import TeamSerializer
 
+logger = logging.getLogger(__name__)
 
 class RegisterTeamView(viewsets.GenericViewSet,
                        mixins.CreateModelMixin):
     serializer_class = TeamSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
+    def perform_create(self, serializer):
+        team = serializer.save()
+
+        # Отправка email
+        subject = 'Регистрация команды'
+        message = f'Команда {team.name} успешно зарегистрирована.'
+        recipient_list = [team.captain.email]
+        try:
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                recipient_list,
+                fail_silently=False
+            )
+        except Exception as e:
+            # Логируем ошибку отправки email
+            logger.error(f"Ошибка отправки email: {e}")
+            # Можем также добавить уведомление администратору о неудачной отправке email, если нужно
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            team = serializer.save()
-
-            # Отправка email
-            subject = 'Регистрация команды'
-            message = f'Команда {team.name} успешно зарегистрирована.'
-            recipient_list = [team.captain.email]
-            try:
-                send_mail(
-                    subject,
-                    message,
-                    settings.DEFAULT_FROM_EMAIL,
-                    recipient_list,
-                    fail_silently=False
-                )
-            except Exception as e:
-                return Response({"detail": "Team registered, but failed to send email."},
-                                status=status.HTTP_201_CREATED)
-
+            self.perform_create(serializer)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
